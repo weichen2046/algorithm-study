5dcb3a775b40dbda373c84c8e231fe8009e66fb3
Chen Wei <weichen2046@gmail.com>
2017-09-04 07:10:44 +0800

    leetcode: new cpp impl of NumberComplement

---
+++ leetcode/cpp/NumberComplement/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+  public:
+    int findComplement(int num)
+    {
+        unsigned mask = ~0;
+        while (num & mask)
+        {
+            mask <<= 1;
+        }
+        return ~mask & ~num;
+    }
+};
